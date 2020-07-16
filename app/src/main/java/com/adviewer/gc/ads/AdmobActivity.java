9c1c1ad99d4f2d14e264ebf4789d1a3e5e6bd597
bort ske <gilbertchoy@gmail.com>
2020-07-16 14:31:31 -0700

    test

--- app/src/main/java/com/adviewer/gc/ads/AdmobActivity.java
+++ app/src/main/java/com/adviewer/gc/ads/AdmobActivity.java
@@ -2,8 +2,8 @@ package com.adviewer.gc.ads;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -13,14 +13,9 @@ import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.InterstitialAd;
-import com.google.android.gms.ads.MobileAds;
 
 public class AdmobActivity extends AppCompatActivity {
 
-    private InterstitialAd mInterstitialAd;
     private Button loadAdBtn;
     private Button playAdBtn;
     private TextView logTextView;
@@ -38,62 +33,20 @@ public class AdmobActivity extends AppCompatActivity {
         logTextView = findViewById(R.id.log1);
         scrollView = findViewById(R.id.scrollview1);
 
-        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
-        //real: ca-app-pub-6760835969070814~5912740615
-        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
-
-        mInterstitialAd = new InterstitialAd(this);
-        mInterstitialAd.setAdUnitId("ca-app-pub-6760835969070814/4954882160");
-
-        //ca-app-pub-6760835969070814/4954882160
-        //real:  ca-app-pub-6760835969070814/4954882160
 
         loadAdBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 log("Load Ad Button Pressed");
-                mInterstitialAd.loadAd(new AdRequest.Builder().build());
             }
         });
 
         playAdBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 log("Play Ad Button Pressed");
-                if (mInterstitialAd.isLoaded()) {
-                    mInterstitialAd.show();
-                } else {
-                    log("Ad not loaded yet");
-                    Log.d("TAG", "The interstitial wasn't loaded yet.");
-                }
 
             }
         });
 
-        mInterstitialAd.setAdListener(new AdListener() {
-            @Override
-            public void onAdLoaded() {
-                log("onAdLoaded");
-            }
-
-            @Override
-            public void onAdFailedToLoad(int errorCode) {
-                log("onAdFailedToLoad");
-            }
-
-            @Override
-            public void onAdOpened() {
-                log("onAdOpened");
-            }
-
-            @Override
-            public void onAdLeftApplication() {
-                log("onAdLeftApplication");
-            }
-
-            @Override
-            public void onAdClosed() {
-                log("onAdClosed");
-            }
-        });
 
     }
 
--- app/src/main/java/com/adviewer/gc/ads/FirstActivity.java
+++ app/src/main/java/com/adviewer/gc/ads/FirstActivity.java
@@ -3,7 +3,7 @@ package com.adviewer.gc.ads;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
